69453854eea8140ecd9fc2d342e66f33c2b16fa6
Hank Wang <drapho@gmail.com>
2018-05-04 23:00:01 +0900

    init commit

---
+++ FlickrViewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlickrViewer
+//
+//  Created by Hank Wang on 2018/5/4.
+//  Copyright © 2018 hanksudo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
